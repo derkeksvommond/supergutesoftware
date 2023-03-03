09b13bea357c0dea533d6c3f04a0845fcadd6a02
Mondkeks <81697210+derkeksvommond@users.noreply.github.com>
2023-03-03 11:16:35 +0100

    Create cockigercock.py

---
+++ cockigercock.py
@@ -0,0 +1,17 @@
+length = int(input())
+
+balls = """
+ /^^^^\_/^^^^\\
+(      |      )
+ \____/^\____/"""
+
+dick = "    |     |"
+
+pit = """    |^^|^^|
+    \__|__/
+"""
+
+print(balls)
+for _ in range(int(length)):
+    print(dick)
+print(pit)
